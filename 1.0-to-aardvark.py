41addda1eccbcd3d86a8c84669a64840d7365fa7
Stephen Appel <srappel930@gmail.com>
2024-03-13 13:57:10 -0500

    force gbl_mdVersion_s to Aardvark

--- 1.0-to-aardvark.py
+++ 1.0-to-aardvark.py
@@ -32,6 +32,9 @@ def schema_update(filepath):
         for old_schema, new_schema in crosswalk.items():
             if old_schema in data:
                 data[new_schema] = data.pop(old_schema)
+
+        # Change the metadata type:
+        data["gbl_mdVersion_s"] = "Aardvark"
             
     # check for multi-valued fields - if so, convert its value to an array
     data = string2array(data)
